b391e88156f288cf75c446d7879d012d6c175803
rivasdiego-dev <rivasdiego.dev@gmail.com>
2024-04-29 05:55:56 -0600

    Update CLI command to include component creation and validation logic

--- src/commands/actions/create.js
+++ src/commands/actions/create.js
@@ -1,6 +1,24 @@
 import chalk from 'chalk';
+import createComponentFiles from '../../utils/createComponentFiles.js';
+import { getAllInternalConfig } from '../../utils/index.js';
+import validatePascalCase from '../../utils/validatePascalCase.js';
 
 export default function createComponent(component, options) {
+
+    const hasConfig = Object.keys(getAllInternalConfig()).length > 0;
+    const isValidComponentName = validatePascalCase(component);
+
+    if (!hasConfig) {
+        console.log(chalk.yellowBright('\nWARNING: No configuration found! 🚨'));
+        console.log(chalk.bold('Use the "init" command to create a new configuration.'));
+        return;
+    }
+
+    if (!isValidComponentName) {
+        console.log(chalk.bold(chalk.redBright('Error:'), 'Invalid component name. Please use PascalCase.'));
+        return;
+    }
+
     const selectedOptionsCount = ['atom', 'molecule', 'organism', 'layout', 'page']
         .map(opt => options[opt])
         .filter(opt => opt)
@@ -10,7 +28,6 @@ export default function createComponent(component, options) {
         console.log(chalk.bold(chalk.redBright('Error:'), 'You must select exactly one component type'));
     } else {
         const selectedType = Object.keys(options).find(opt => options[opt]);
-        console.log(`Creating ${selectedType} component named "${component}"...`);
-        // TODO: Implement component creation logic
+        createComponentFiles(component, selectedType);
     }
 }

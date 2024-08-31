fc99e672e1b4edd58d0c754dca15369c622fcd83
rivasdiego-dev <rivasdiego.dev@gmail.com>
2024-08-31 12:46:55 -0600

    Update CLI command to use 'atomd' instead of 'atomicd' and bumped version
    The CLI command has been changed from 'atomicd' to 'atomd' to align with the project's new naming convention. This update helps to improve consistency and ensures that the command is easier to remember and type.

--- bin/global.js
+++ bin/global.js
@@ -8,15 +8,15 @@ import addConfigCommand from "../src/commands/config.js";
 const program = new Command();
 
 program
-    .name("atomicd")
+    .name("atomd")
     .description("A CLI tool to generate files using Atomic Desing convention.\nRead more at:\n\t- https://atomicdesign.bradfrost.com/\n\t- https://bradfrost.com/blog/post/atomic-web-design/")
-    .version("1.0.0");
-
-// Create command
-addCreateCommand(program);
+    .version("1.1.0");
 
 // Init command
 addInitCommand(program);
+
+// Create command
+addCreateCommand(program);
 
 // Config command
 addConfigCommand(program);
